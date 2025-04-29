2260d9e84af0a2ec26b56843e51e991fdf13105c
David_Creat <80436092+DavidCreat@users.noreply.github.com>
2025-04-29 05:34:31 -0500

    api.py 0.2

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ from solucionador_ecuaciones import analizar_ecuacion, metodo_biseccion, metodo_
 from flask_cors import CORS
 import traceback
 import math
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -102,4 +103,6 @@ def home():
     return 'API de solucionador de ecuaciones funcionando', 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
